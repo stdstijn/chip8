ef228cb0f8936b9495daa8132946c28b9bf82305
Stijn Eijndhoven <s.eijndhoven@outlook.com>
2023-05-11 22:42:38 +0200

    Fix OP_Fx0A

--- src/chip8.c
+++ src/chip8.c
@@ -412,23 +412,27 @@ void OP_Fx0A(CPU* cpu) // LD Vx, K
 {
     uint8_t x = (cpu->opcode & 0x0F00u) >> 8u;
 
-    if (cpu->key[0x0]) cpu->v[x] = 0;
-    else if (cpu->key[0x1]) cpu->v[x] = 0x1;
-    else if (cpu->key[0x2]) cpu->v[x] = 0x2;
-    else if (cpu->key[0x3]) cpu->v[x] = 0x3;
-    else if (cpu->key[0x4]) cpu->v[x] = 0x4;
-    else if (cpu->key[0x5]) cpu->v[x] = 0x5;
-    else if (cpu->key[0x6]) cpu->v[x] = 0x6;
-    else if (cpu->key[0x7]) cpu->v[x] = 0x7;
-    else if (cpu->key[0x8]) cpu->v[x] = 0x8;
-    else if (cpu->key[0x9]) cpu->v[x] = 0x9;
-    else if (cpu->key[0xA]) cpu->v[x] = 0xA;
-    else if (cpu->key[0xB]) cpu->v[x] = 0xB;
-    else if (cpu->key[0xC]) cpu->v[x] = 0xC;
-    else if (cpu->key[0xD]) cpu->v[x] = 0xD;
-    else if (cpu->key[0xE]) cpu->v[x] = 0xE;
-    else if (cpu->key[0xF]) cpu->v[x] = 0xF;
+    static uint8_t keyPressed[0xF];
+
+    if (cpu->key[0x0] && keyPressed[0x0]) cpu->v[x] = 0;
+    else if (!cpu->key[0x1] && keyPressed[0x1]) cpu->v[x] = 0x1;
+    else if (!cpu->key[0x2] && keyPressed[0x2]) cpu->v[x] = 0x2;
+    else if (!cpu->key[0x3] && keyPressed[0x3]) cpu->v[x] = 0x3;
+    else if (!cpu->key[0x4] && keyPressed[0x4]) cpu->v[x] = 0x4;
+    else if (!cpu->key[0x5] && keyPressed[0x5]) cpu->v[x] = 0x5;
+    else if (!cpu->key[0x6] && keyPressed[0x6]) cpu->v[x] = 0x6;
+    else if (!cpu->key[0x7] && keyPressed[0x7]) cpu->v[x] = 0x7;
+    else if (!cpu->key[0x8] && keyPressed[0x8]) cpu->v[x] = 0x8;
+    else if (!cpu->key[0x9] && keyPressed[0x9]) cpu->v[x] = 0x9;
+    else if (!cpu->key[0xA] && keyPressed[0xA]) cpu->v[x] = 0xA;
+    else if (!cpu->key[0xB] && keyPressed[0xB]) cpu->v[x] = 0xB;
+    else if (!cpu->key[0xC] && keyPressed[0xC]) cpu->v[x] = 0xC;
+    else if (!cpu->key[0xD] && keyPressed[0xD]) cpu->v[x] = 0xD;
+    else if (!cpu->key[0xE] && keyPressed[0xE]) cpu->v[x] = 0xE;
+    else if (!cpu->key[0xF] && keyPressed[0xF]) cpu->v[x] = 0xF;
     else cpu->pc -= 2;
+
+    copyMemory(keyPressed, cpu->key, sizeof(cpu->key));
 }
 
 void OP_Fx15(CPU* cpu) // LD DT, Vx
