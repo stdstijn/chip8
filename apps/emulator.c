7b7e955c77623f2369dbfd01e06aa11c093af467
Stijn Eijndhoven <s.eijndhoven@outlook.com>
2023-05-11 16:00:57 +0200

    Removed switch case

--- apps/emulator.c
+++ apps/emulator.c
@@ -136,9 +136,13 @@ void PlatformUpdate(Platform* p, const void* buffer)
 
 int PlatformProcessInput(uint8_t* keys)
 {
-    int quit = 0;
-
+        int quit = 0;
     SDL_Event event;
+
+    static const uint8_t keyMap[] = {
+        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+        0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF
+    };
 
     while (SDL_PollEvent(&event))
     {
@@ -148,113 +152,14 @@ int PlatformProcessInput(uint8_t* keys)
             quit = 1;
             break;
         case SDL_KEYDOWN:
-            switch (event.key.keysym.sym)
-            {
-            case SDLK_ESCAPE:
+            if (event.key.keysym.sym == SDLK_ESCAPE) 
                 quit = 1;
-                break;
-            case SDLK_x:
-                keys[0] = 1;
-                break;
-            case SDLK_1:
-                keys[1] = 1;
-                break;
-            case SDLK_2:
-                keys[2] = 1;
-                break;
-            case SDLK_3:
-                keys[3] = 1;
-                break;
-            case SDLK_q:
-                keys[4] = 1;
-                break;
-            case SDLK_w:
-                keys[5] = 1;
-                break;
-            case SDLK_e:
-                keys[6] = 1;
-                break;
-            case SDLK_a:
-                keys[7] = 1;
-                break;
-            case SDLK_s:
-                keys[8] = 1;
-                break;
-            case SDLK_d:
-                keys[9] = 1;
-                break;
-            case SDLK_z:
-                keys[0xA] = 1;
-                break;
-            case SDLK_c:
-                keys[0xB] = 1;
-                break;
-            case SDLK_4:
-                keys[0xC] = 1;
-                break;
-            case SDLK_r:
-                keys[0xD] = 1;
-                break;
-            case SDLK_f:
-                keys[0xE] = 1;
-                break;
-            case SDLK_v:
-                keys[0xF] = 1;
-                break;
-            }
+            if (event.key.keysym.sym < 128) 
+                keys[keyMap[event.key.keysym.sym]] = 1;
             break;
         case SDL_KEYUP:
-            switch (event.key.keysym.sym)
-            {
-            case SDLK_x:
-                keys[0] = 0;
-                break;
-            case SDLK_1:
-                keys[1] = 0;
-                break;
-            case SDLK_2:
-                keys[2] = 0;
-                break;
-            case SDLK_3:
-                keys[3] = 0;
-                break;
-            case SDLK_q:
-                keys[4] = 0;
-                break;
-            case SDLK_w:
-                keys[5] = 0;
-                break;
-            case SDLK_e:
-                keys[6] = 0;
-                break;
-            case SDLK_a:
-                keys[7] = 0;
-                break;
-            case SDLK_s:
-                keys[8] = 0;
-                break;
-            case SDLK_d:
-                keys[9] = 0;
-                break;
-            case SDLK_z:
-                keys[0xA] = 0;
-                break;
-            case SDLK_c:
-                keys[0xB] = 0;
-                break;
-            case SDLK_4:
-                keys[0xC] = 0;
-                break;
-            case SDLK_r:
-                keys[0xD] = 0;
-                break;
-            case SDLK_f:
-                keys[0xE] = 0;
-                break;
-            case SDLK_v:
-                keys[0xF] = 0;
-                break;
-            }
+            if (event.key.keysym.sym < 128) 
+                keys[keyMap[event.key.keysym.sym]] = 0;
             break;
         }
     }
